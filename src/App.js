8f85c36957750b99828a2cf1b1fabe989152c8bb
EastZero123 <99165555+EastZero123@users.noreply.github.com>
2023-03-08 16:16:23 +0900

    happyict

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Footer from "./component/common/Footer"
+import Header from "./component/common/Header"
+import { Route, Routes, useLocation } from "react-router-dom"
+import Intro from "./pages/Intro"
+import Contact from "./pages/Contact"
+import MainPage from "./pages/Main"
 
 function App() {
+  const location = useLocation()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <body className="home">
+      <ul id="skip-navi">
+        <li>
+          <a href="#content">본문 바로가기</a>
+        </li>
+      </ul>
+      {/* <!-- wrap --> */}
+      <div className="wrap">
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<MainPage pathname={location.pathname} />} />
+          {/* <!-- // container --> */}
+          <Route path="/company/intro" element={<Intro />} />
+          <Route path="/company/map" element={<Contact />} />
+        </Routes>
+        {/* <!-- footer --> */}
+        <Footer />
+        {/* <!-- //footer --> */}
+      </div>
+      {/* <!-- //wrap --> */}
+    </body>
+  )
 }
 
-export default App;
+export default App
